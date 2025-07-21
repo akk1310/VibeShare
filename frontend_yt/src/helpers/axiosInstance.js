a72428ae52db44137bcb03e03f3a28ca638fdc14
akk1310 <kundlikadnan64@gmail.com>
2025-07-21 12:35:04 +0530

    Add Axios interceptor for auth token using persist:root

--- frontend_yt/src/helpers/axiosInstance.js
+++ frontend_yt/src/helpers/axiosInstance.js
@@ -5,4 +5,24 @@ const axiosInstance = axios.create();
 axiosInstance.defaults.baseURL = BASE_URL;
 axiosInstance.defaults.withCredentials = true;
 
+axiosInstance.interceptors.request.use((config) => {
+  try {
+    const persistedRoot = localStorage.getItem("persist:root");
+    if (persistedRoot) {
+      const authState = JSON.parse(persistedRoot).auth;
+      const userData = JSON.parse(authState).userData;
+
+      const accessToken = userData?.accessToken;
+
+      if (accessToken) {
+        config.headers.Authorization = `Bearer ${accessToken}`;
+      }
+    }
+  } catch (err) {
+    console.error("Token extraction error", err);
+  }
+
+  return config;
+});
+
 export default axiosInstance;
